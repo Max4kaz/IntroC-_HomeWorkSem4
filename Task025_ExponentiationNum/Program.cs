64570c11405a95cdab2597722e32ded40805c594
Maxim <max4kaz@gmail.com>
2022-07-29 20:58:35 +0700

    Add all tasks homework of Seminar 4

---
+++ Task025_ExponentiationNum/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int number = InputInt("Введите число А: ");
+int degree = InputInt("Введите число В: ");
+int result = 1;
+
+for (int i = 1; i <= degree; i++)
+{
+    result *= number;
+}
+Console.WriteLine($"Результат возведения числа А в натуральную степень В равен {result}");
---
+++ Task029_ArrayEightNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] arrayEightNum = new int[8];
+
+for (int i = 0; i < arrayEightNum.Length; i++)
+{
+    arrayEightNum[i] = new Random().Next(0, 100);
+}
+PrintArray(arrayEightNum);
